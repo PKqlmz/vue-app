c0f899308b53e24b5984c48b0b1699ec304c61cd
PKqlmz <1981806184@qq.com>
2018-11-10 18:42:47 +0800

    again

---
+++ src/components/news/newsIfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div> 
+        <div class="div">
+        {{msq + '---------' +  id}} 
+        </div>
+        </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msq : '能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办',
+            id : this.$route.params.id          //获取id
+        }
+    }
+}
+</script>
+<style>
+.div {
+    margin: 20px 40px;
+    color: #226eff;
+}
+</style>
---
+++ src/components/news/newsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media">
+					<router-link :to="'/home/newsIfo/' +1">
+						<img class="mui-media-object mui-pull-left" src="http://pic29.nipic.com/20130511/9252150_174018365301_2.jpg">
+						<div class="mui-media-body">
+							<h1>能和心爱的人一起睡觉，是件幸福的事情；</h1>
+							<p class='mui-ellipsis'>
+                                <span>发表时间：2018年11月10日</span>
+                                <span>点击次数：5</span>
+                            </p>
+						</div>
+					</router-link>
+				</li>
+				
+			</ul>
+</template>
+<script>
+
+</script>
+<style>
+.mui-table-view li h1{
+font-size: 14px;
+color : #226aff
+}
+.mui-table-view li .mui-media-body .mui-ellipsis {
+display: flex;
+justify-content: space-between;
+}
+</style>
